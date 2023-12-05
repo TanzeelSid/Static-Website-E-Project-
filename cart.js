a4fc26e1b886ca758f19f841d7524af3fc0e736c
Tanzeel Siddique <tnzlsiddique@gmail.com>
2023-12-05 14:49:41 +0500

    upload project

---
+++ cart.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    $('#btt').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+
+    $('#btt1').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt2').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt3').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt4').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt5').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt6').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt7').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+    $('#btt8').click(function(){
+        var i;
+    i=$('#bd').text();
+    i=Number(i)+1;
+ $('#bd').text(i);
+    (  i +' item is added to cart ' )
+    })
+   
+
+
+
+
+});
